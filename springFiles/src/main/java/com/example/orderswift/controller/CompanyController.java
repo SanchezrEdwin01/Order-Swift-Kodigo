3102075a95b9a00b9033b7048d5c648794f2d5bd
wilhelm <vg162762@alumno.udb.edu.sv>
2023-05-09 14:05:53 -0600

    Update & Delete Companies has been added

--- springFiles/src/main/java/com/example/orderswift/controller/CompanyController.java
+++ springFiles/src/main/java/com/example/orderswift/controller/CompanyController.java
@@ -2,6 +2,7 @@ package com.example.orderswift.controller;
 
 import com.example.orderswift.model.Company;
 import com.example.orderswift.service.CompanyService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,12 +11,9 @@ import java.util.List;
 
 @RestController
 @RequestMapping("company")
+@RequiredArgsConstructor
 public class CompanyController {
     private final CompanyService companyService;
-
-    public CompanyController(CompanyService companyService) {
-        this.companyService = companyService;
-    }
 
     @GetMapping
     public List<Company> getCompanies(){
@@ -30,5 +28,16 @@ public class CompanyController {
     @PostMapping("add")
     public ResponseEntity<Company> addCompany(@RequestBody Company company){
         return new ResponseEntity<>(companyService.createCompany(company), HttpStatus.CREATED);
+    }
+
+    @PutMapping("{companyId}")
+    public ResponseEntity<Company> editCompany(@PathVariable Integer companyId,
+                                               @RequestBody Company company){
+        return new ResponseEntity<>(companyService.updateCompany(companyId, company), HttpStatus.OK);
+    }
+
+    @DeleteMapping("{companyId}")
+    public ResponseEntity<String> removeCompany(@PathVariable Integer companyId){
+        return new ResponseEntity<>(companyService.deleteCompany(companyId), HttpStatus.NO_CONTENT);
     }
 }
